7780cc47d24fd52e55f0c5a51972ea48f75a71ee
Nick <nicholas_petrocelli@brown.edu>
2022-10-26 22:42:54 -0400

    ugh please work

--- create_venv.sh
+++ create_venv.sh
@@ -4,6 +4,6 @@ python3 -m pip install virtualenv
 python3 -m virtualenv --python=python3.7 electra_env
 source electra_env/bin/activate
 pip install -U pip
-pip install -r requirements.txt
+pip install -r --default-timeout=1000 requirements.txt
 
 echo environment ready, activate with "source electra_env/bin/activate" next time you log in
