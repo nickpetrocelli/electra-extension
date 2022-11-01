d5e7fc6a88c1397e47f390d32c29e4f3e6c33287
Nicholas Petrocelli <npetroce@cs.brown.edu>
2022-11-01 16:15:29 -0400

    fixed py env

--- create_venv.sh
+++ create_venv.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+#export LD_LIBRARY_PATH=/local/projects/python3.7/root-3.7.15/lib
 python3 -m pip install virtualenv
-python3 -m virtualenv --python=python3.7 electra_env
+python3 -m virtualenv --python="/local/projects/python3.7/root-3.7.15/bin/python3.7" electra_env
 source electra_env/bin/activate
 pip install -U pip
-pip install -r --default-timeout=1000 requirements.txt
+pip install --default-timeout=1000 -r  requirements.txt
 
 echo environment ready, activate with "source electra_env/bin/activate" next time you log in
